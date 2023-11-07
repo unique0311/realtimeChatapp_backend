d24d60ab2df1582d8f9a133976ef6b523d4ada66
fredgeorge0311 <george.fred0311@gmail.com>
2023-11-06 20:59:17 -0500

    completed server

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 
 const harperSaveMessage = require("./services/harper-save-message");
 const harperGetMessages = require("./services/harper-get-messages");
+const leaveRoom = require("./utils/leave-room");
 
 app.use(cors());
 const server = http.createServer(app);
@@ -64,6 +65,32 @@ io.on("connection", (socket) => {
         socket.emit("last_100_messages", last100Messages);
       })
       .catch((err) => console.log(err));
+  });
+
+  socket.on("leave_room", (data) => {
+    const { username, room } = data;
+    socket.leave(room);
+    const __createdtime__ = Date.now();
+    allUsers = leaveRoom(socket.id, allUsers);
+    socket.to(room).emit("chatroom_users", allUsers);
+    socket.to(room).emit("receive_message", {
+      username: CHAT_BOT,
+      message: `${username} has left the chat`,
+      __createdtime__,
+    });
+    console.log(`${username} has left the chat`);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected from the chat");
+    const user = allUsers.find((user) => user.id == socket.id);
+    if (user?.username) {
+      allUsers = leaveRoom(socket.id, allUsers);
+      socket.to(chatRoom).emit("chatroom_users", allUsers);
+      socket.to(chatRoom).emit("receive_message", {
+        message: `${user.username} has disconnected from the chat.`,
+      });
+    }
   });
 });
 
